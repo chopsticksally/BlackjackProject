0e0fc08bdad83f62c62718aada24dfe685e3274a
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-03-17 21:10:47 -0600

    BlackJack refactored file names

--- src/com/skilldistillery/blackjack/GameApp.java
+++ src/com/skilldistillery/blackjack/GameApp.java
@@ -3,92 +3,12 @@ package com.skilldistillery.blackjack;
 import java.util.Scanner;
 
 public class GameApp {
-	Dealer dealer = new Dealer();
-	Player player = new Player();
 
-	public void launch(Scanner kb) {
-		dealer.getDeck().shuffleDeck();
-		System.out.println("Do you want to play a round of Black Jack? Y/N");
-		String ans = kb.next();
-		if (ans.equalsIgnoreCase("N")) {
-			System.out.println("Thanks, Goodbye!");
-			System.exit(0);
-		}
-		if (dealer.getDeck().checkDeckSize() < 10) {
-			dealer.getNewDeck();
-		}
-		player.getHand().addCard(dealer.getDeck().dealCard());
-		dealer.getHand().addCard(dealer.getDeck().dealCard());
-
-		player.getHand().addCard(dealer.getDeck().dealCard());
-		dealer.getHand().addCard(dealer.getDeck().dealCard());
-
-		System.out.println("Your hand: " + player.getHand());
-		System.out.println("Your total hand is: " + player.getHand().getHandValue());
-		System.out.println("Dealer shows: " + dealer.getHand().displayTopCard());
-		System.out.println("Hit Y/N: ");
-		String choice = kb.next();
-
-		while (choice.equalsIgnoreCase("Y")) {
-			playerHit();
-			System.out.println("Your hand: " + player.getHand());
-			System.out.println("Your total hand is now: " + player.getHand().getHandValue());
-			if (player.getHand().getHandValue() > 21) {
-				System.out.println("You Bust with " + player.getHand().getHandValue() + " , Dealer wins");
-				resetGame(kb);
-			} else if (player.getHand().getHandValue() == 21) {
-				System.out.println("YOU WIN with " + player.getHand().getHandValue());
-				resetGame(kb);
-			}
-			System.out.println("Hit Y/N: ");
-			choice = kb.next();
-
-		}
-		if (choice.equalsIgnoreCase("N")) {
-			System.out.println("Dealer has :" + dealer.getHand());
-			System.out.println("Dealer shows: " + dealer.getHand().getHandValue());
-
-			while (dealer.getHand().getHandValue() <= 17) {
-				System.out.println("dealer hitting");
-				dealerHit();
-				System.out.println("Dealer now has :" + dealer.getHand());
-				System.out.println("Dealer shows: " + dealer.getHand().getHandValue());
-
-				if (dealer.getHand().getHandValue() > 21) {
-					System.out.println("Dealer busts, YOU WIN!");
-					resetGame(kb);
-
-				}
-
-			}
-			if (dealer.getHand().getHandValue() > player.getHand().getHandValue()) {
-				System.out.println("Dealer Wins with " + dealer.getHand().getHandValue());
-				resetGame(kb);
-
-			} else if (dealer.getHand().getHandValue() < player.getHand().getHandValue()) {
-				System.out.println("YOU WIN with " + player.getHand().getHandValue());
-				resetGame(kb);
-
-			} else if (dealer.getHand().getHandValue() == player.getHand().getHandValue()) {
-				System.out.println("Push");
-				resetGame(kb);
-			}
-		}
-	}
-
-	public void playerHit() {
-		player.getHand().addCard(dealer.getDeck().dealCard());
-	}
-
-	public void dealerHit() {
-		dealer.getHand().addCard(dealer.getDeck().dealCard());
-
-	}
-
-	public void resetGame(Scanner kb) {
-		dealer.getHand().clearHand();
-		player.getHand().clearHand();
-		launch(kb);
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		Game game = new Game();
+		game.launch(kb);
+		kb.close();
 	}
 
 }
